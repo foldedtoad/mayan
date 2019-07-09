5e64bb7c2528eacbccc4628f7790c78e732a184d
foldedtoad <foldedtoad@gmail.com>
2019-07-08 20:58:34 -0700

    minor changes

--- python/editor/editor.py
+++ python/editor/editor.py
@@ -10,17 +10,20 @@ from os.path import isfile, join
 
 glyphs = {'glyphs': []}
 
+imagesPath = './images/'
+jsonFilename = 'glyphs.json'
 
 def printGlyphs():
 	for item in glyphs['glyphs']:
-		print "filename: {0}, length: {1:>6}, data: {2:40.40}...".format(item['name'], str(item['len']), item['data'])
+		print "filename: {0}, length: {1:>6}, data: {2:40.40}..., {3}, {4}".format(
+			item['name'], str(item['len']), item['data'], item['mayan'], item['latin'])
 
 
 def loadGlyphs():
 	try:
-		with open('glyphs.json') as json_file:  
-			glyphs = json.load(json_file)
-			json_file.close()
+		with open(jsonFilename) as file:  
+			glyphs = json.load(file)
+			file.close()
 		print 'loaded JSON'
 	except:
 		print 'create JSON'
@@ -29,7 +32,7 @@ def loadGlyphs():
 
 def saveGlyphs():
 	try:
-		with open('glyphs.json', 'w') as outfile:
+		with open(jsonFilename, 'w') as outfile:
 			json.dump(glyphs, outfile, indent=4)
 		outfile.close()
 	except:
@@ -38,23 +41,24 @@ def saveGlyphs():
 
 
 def addGlyph(filename):
-	with open('./images/' + filename, mode='rb') as file:
+	with open(imagesPath + filename, mode='rb') as file:
 		data = file.read();
 		length = len(data)
 		content = data.encode('base64')
 		name, extension = os.path.splitext(filename)
 
-		glyphs['glyphs'].append({'name':name, 'len': length, 'data': content})
+		glyphs['glyphs'].append({'name':name, 'len': length, 'data': content, 'mayan': 'n/a', 'latin': 'n/a'})
 		file.close()
+
 
 if __name__ == '__main__':
 
-	print "core main start"
+	print "editor start"
 
 	loadGlyphs()
 
 	filesList = []
-	for root, dir, files in os.walk('./images'):
+	for root, dir, files in os.walk(imagesPath):
 		for filename in files:
 			if '.png' in filename.lower():
 				filesList.append(os.path.join('', filename))
@@ -67,8 +71,6 @@ if __name__ == '__main__':
 
 	saveGlyphs()
 
-	print "core main done"
+	print "editor end"
 
 	sys.exit(0)
-
-
